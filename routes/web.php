73939e5f96677c3cb084a07c263aced42ec8c08b
Nicolas KLEIN <nicolas6720@gmail.com>
2023-10-01 14:35:13 +0200

    url matching

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/blog", function () {
+    return "Hello World";
+});
+
+Route::get("/blog/{slug}-{id}", function (String $slug, String $id) {
+    return [
+        "slug" => $slug,
+        "id" => $id
+    ];
+})->where(
+    [
+        "slug" => "[A-Za-z\-]+",
+        "id" => "[0-9]+"
+    ]
+);
